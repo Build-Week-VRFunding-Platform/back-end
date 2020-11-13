40de2e13710c9ab6e54f6495daeb188ce58a4acb
willwearing <willwearing@gmail.com>
2020-11-13 15:50:28 -0700

    mvp - trying to get heroku working

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ require("colors");
 const server = require("./server.js");
 
 const PORT = process.env.PORT || 5000;
+const secret = process.env.SECRET_THING || "foo";
+
+console.log(PORT, secret);
 
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`.random);
