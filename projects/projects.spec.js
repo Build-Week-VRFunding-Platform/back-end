8e0eecb63293b5c5522a684ecc3b649e0fede616
willwearing <willwearing@gmail.com>
2020-11-18 10:03:56 -0700

    changing delete

---
+++ projects/projects.spec.js
@@ -0,0 +1,5 @@
+describe("test works", () => {
+  test("it works", () => {
+    expect(1 + 1).toBe(2);
+  });
+});
---
+++ users/user.spec.js
@@ -0,0 +1,28 @@
+const User = require("./user-model");
+const db = require("../database/db-config");
+
+describe("user test works", () => {
+  test("it works", () => {
+    expect(1 + 1).toBe(2);
+  });
+});
+
+describe("modules work", () => {
+  beforeEach(async () => {
+    await db("projects").truncate();
+    await db("users").truncate();
+  });
+
+  describe("add method works", () => {
+    it("will insert a new user", async () => {
+      await User.add({
+        email: "random@random.com",
+        password: "123456789",
+        name: "test123",
+        role: 0,
+      });
+      const users = await db("users");
+      expect(users).toHaveLength(1);
+    });
+  });
+});
